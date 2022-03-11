dcc61119159f2bde90a25705a88271e36fb5c991
heejeong-choi <jennachoi27@gmail.com>
2022-03-11 15:44:19 +0900

    문자열 추가

---
+++ strings.py
@@ -0,0 +1,30 @@
+print("hello world \t no")
+print("Hello World")
+print('Python is Fun')
+print("""Life is too short, You need Python!""")
+print('''Life is too short, You need Python!''')
+
+# 문자열에 작은따옴표 포함시키기
+food = "Python's favorite food is perl"
+print(food)
+
+# 문자열에 큰따옴표 포함시키기
+statement = '"Python is very easy." he said.'
+print(statement)
+
+# 백슬래시 사용하기
+food = 'Python\'s favorite food is pizza'
+print(food)
+
+# 백슬래시 사용하지 않기 위해 따옴표 3개 사용하기
+multiline = '''
+life is too short
+you need python 
+'''
+print(multiline)
+multiline = """
+why does it work?
+i dont understand
+because i am java developer
+"""
+print(multiline)
